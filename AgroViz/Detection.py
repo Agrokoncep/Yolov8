00252f3549b9820a3b684c3332d38b7f139c0caf
Alexandre Durand <alexdurand12@hotmail.com>
2024-01-30 14:29:18 -0500

    modification to add freeze

--- AgroViz/Detection.py
+++ AgroViz/Detection.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from PIL import Image
 import cv2
 
-model = YOLO("D:\\Datasets\\Agrokoncept\\Training\\exalted-durian-8\\best.pt")
+model = YOLO("B:\\Datasets\\Agrokoncep\\Models\\tomato_n.pt")
 # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
 # results = model.predict(source="0")
 # results = model.predict(source="folder", show=True) # Display preds. Accepts all YOLO predict arguments
@@ -10,3 +10,4 @@ model = YOLO("D:\\Datasets\\Agrokoncept\\Training\\exalted-durian-8\\best.pt")
 # from ndarray
 im2 = cv2.imread("depositphotos_336989494-stock-photo-tomatoes-growing-in-a-greenhouse.jpg")
 results = model.predict(source=im2, save=True, save_txt=True, )  # save predictions as labels
+print(results)
--- AgroViz/train.py
+++ AgroViz/train.py
@@ -1,17 +1,29 @@
 from ultralytics import YOLO
 import wandb
 
-wandb.login(key="17db354b20a284ceacef610b63e16effc00b0ff3")
-wandb.init(project="agrokoncept-tomates")
-# Load a model
-model = YOLO('yolov8n.pt')
-
-# Load custom datasets :
-dataset_path = "D:\\Datasets\\Agrokoncept\\TomatoesMergedYolov8\\tomatoes.yaml"
-
+def freeze_layer(trainer):
+    model = trainer.model
+    num_freeze = 10
+    print(f"Freezing {num_freeze} layers")
+    freeze = [f'model.{x}.' for x in range(num_freeze)]  # layers to freeze
+    for k, v in model.named_parameters():
+        v.requires_grad = True  # train all layers
+        if any(x in k for x in freeze):
+            print(f'freezing {k}')
+            v.requires_grad = False
+    print(f"{num_freeze} layers are freezed.")
 
 if __name__ == '__main__':
+    wandb.login(key="17db354b20a284ceacef610b63e16effc00b0ff3")
+    wandb.init(project="agrokoncep-tomato")
+    # Load a model
+    model = YOLO('yolov8m.pt')
+    model.add_callback("on_train_start", freeze_layer)
+    # Load custom datasets :
+    dataset_path = "E:\\Batch#1\\data.yaml"
+
     # Train the model
-    result_train = model.train(data=dataset_path, epochs=100, imgsz=640, batch=16, workers=2, name="test_tomatoes")
+    result_train = model.train(data=dataset_path, epochs=300, imgsz=1280, batch=8, workers=2,
+                               name="test_tomatoes", lr0=0.05,device="0", patience=75)
     # Evaluate the model's performance on the validation set
     results_val = model.val()

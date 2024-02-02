02fbc13125acee305eb373f9c9a588a35433c7f3
Blain354 <gblain03@hotmail.com>
2024-02-02 09:35:41 -0500

    refactor config

---
+++ AgroViz/config.py
@@ -0,0 +1,36 @@
+import os
+
+MODEL_SIZE = dict(
+    NANO = "yolov8n.pt",
+    SMALL = "yolov8s.pt",
+    MEDIUM = "yolov8m.pt",
+    LARGE = "yolov8l.pt",
+    XLARGE = "yolov8x.pt",
+)
+
+wandb = dict(
+    api_key = '0a172795af06b84dd255b5192d3722a9d098af32',
+    project_name = 'batch1',
+)
+
+data = dict(
+    batch_datasets_folder = "C:\\Users\\gblai\\Documents\\github\\AgroKoncep\\Dataset",
+    dataset_name = "baseimg-batch1-augmented1",
+    dataset_config_name = "data.yaml",
+)
+
+dataset_abs_path = os.path.join(data["batch_datasets_folder"], data["dataset_name"], data["dataset_config_name"])
+
+model_size = MODEL_SIZE["MEDIUM"]  # Choose from MODEL_SIZE,
+
+train_kwargs = dict(
+    data = dataset_abs_path,
+    name = f"{data['dataset_name']}_",
+    epochs = 300,
+    imgsz = 640,
+    batch = 16,
+    workers = 8,
+    lr0 = 0.07,
+    device = "0",
+    patience = 75,
+)
--- AgroViz/train.py
+++ AgroViz/train.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import wandb
+import os
+import config
 
 def freeze_layer(trainer):
     model = trainer.model
@@ -14,16 +16,14 @@ def freeze_layer(trainer):
     print(f"{num_freeze} layers are freezed.")
 
 if __name__ == '__main__':
-    wandb.login(key="17db354b20a284ceacef610b63e16effc00b0ff3")
-    wandb.init(project="agrokoncep-tomato")
+    wandb.login(key=config.wandb.get("api_key"))
+    wandb.init(project=config.wandb.get("project_name"))
     # Load a model
-    model = YOLO('yolov8m.pt')
+    model = YOLO(config.model_size)
     model.add_callback("on_train_start", freeze_layer)
-    # Load custom datasets :
-    dataset_path = "E:\\Batch#1\\data.yaml"
 
+    print(f"Dataset path: {config.dataset_abs_path}")
     # Train the model
-    result_train = model.train(data=dataset_path, epochs=300, imgsz=1280, batch=8, workers=2,
-                               name="test_tomatoes", lr0=0.05,device="0", patience=75)
+    result_train = model.train(**config.train_kwargs)
     # Evaluate the model's performance on the validation set
     results_val = model.val()

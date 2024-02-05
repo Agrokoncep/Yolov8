45a8d44047476583248220f903160f15132c2694
Blain354 <gblain03@hot5mail.com>
2024-02-04 21:02:10 -0500

    display modif

--- AgroViz/train.py
+++ AgroViz/train.py
@@ -5,20 +5,25 @@ import config
 
 def freeze_layer(trainer):
     model = trainer.model
-    config.nb_freezed_layers = 5
-    print(f"Freezing {config.nb_freezed_layer} layers")
-    freeze = [f'model.{x}.' for x in range(config.nb_freezed_layer)]  # layers to freeze
+    print(f"Freezing {config.nb_freezed_layers} layers")
+    freeze = [f'model.{x}.' for x in range(config.nb_freezed_layers)]  # layers to freeze
     for k, v in model.named_parameters():
         v.requires_grad = True  # train all layers
         if any(x in k for x in freeze):
             print(f'freezing {k}')
             v.requires_grad = False
-    print(f"{config.nb_freezed_layer} layers are freezed.")
+    print(f"{config.nb_freezed_layers} layers are freezed.")
 
 if __name__ == '__main__':
     wandb.login(key=config.wandb.get("api_key"))
     wandb.init(project=config.wandb.get("project_name"), 
-               notes=f"Dataset={config.data.get('dataset_name')} // model_size={config.model_size} // lr0={config.train_kwargs.get('lr0')} // batch={config.train_kwargs.get('batch')} // epochs={config.train_kwargs.get('epochs')} // workers={config.train_kwargs.get('workers')} // n_freeze_layers={config.nb_freezed_layers}",
+               notes=f"""Dataset={config.data.get('dataset_name')} 
+                // model_size={config.model_size} 
+                // lr0={config.train_kwargs.get('lr0')} 
+                // batch={config.train_kwargs.get('batch')} 
+                // epochs={config.train_kwargs.get('epochs')}  
+                // workers={config.train_kwargs.get('workers')} 
+                // n_freeze_layers={config.nb_freezed_layers}""",
     )
     # Load a model
     model = YOLO(config.model_size)
